39b9df59952be922b2f33679aaa29bd58bdf8315
marioperna <pernamario.bus@gmail.com>
2018-10-17 08:54:23 +0200

    Upgrande button random

--- src/App.js
+++ src/App.js
@@ -1,41 +1,33 @@
 import React, { Component } from 'react';
-import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      term: '',
-      img: ''
-    };
+  state = {
+    counter: 0,
+    random:0
   }
-
-  onChange = (event) => {
-    this.setState({ term: event.target.value });
-  }
-  handleSubmit = (event) => {
-    event.preventDefault();
-    const api_key = 'dc6zaTOxFJmzC';
-    const url = `http://api.giphy.com/v1/gifs/search?q=${this.state.term}&api_key=${api_key}`;
-    fetch(url)
-      .then(response => response.json())
-      .then(data => this.setState({ term:'', img: data.data[0].images.fixed_height.url }))
-      .catch(e => console.log('error', e));
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <form onSubmit={this.handleSubmit}>
-          <input value={this.state.term} onChange={this.onChange} />
-          <button>Search!</button>
-        </form>
-        <img src={this.state.img} height="200" alt={this.state.term} />
-      </div>
-    );
+  render(){
+    var min = 1;
+    var max = 100;
+    var rand =  min + (Math.random() * (max-min));
+  
+  
+    return(
+        <div>
+        
+          <h1>Counter: {this.state.counter} </h1>
+          <h2>Counter: {this.state.random} </h2>
+          <button onClick={()=> this.setState({counter: this.state.counter+1})}>
+            Increment
+          </button>
+          <button onClick={()=> this.setState({counter: this.state.counter-1})}>
+            Decrement
+          </button>
+          <button onClick={()=> this.setState({random: rand})}>
+            Random
+          </button>        
+        </div>
+      );
   }
 }
-
-
 
 export default App;
